2106bd4b403f84706de78d49572c2f642bd67d99
Nick Escobedo <nick@nickescobedo.com>
2014-11-23 23:48:40 -0600

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,5 @@
 <?php
 error_reporting(E_ALL | E_STRICT);
 
-require_once dirname(dirname(__FILE__)) . '/../../../vendor/autoload.php';
+$loader = require 'vendor/autoload.php';
+$loader->add('nickescobedo\\penapi\\', __DIR__);
