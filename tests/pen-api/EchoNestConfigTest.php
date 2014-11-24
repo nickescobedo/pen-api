3ff60e44fa89ae641db17a8c230201e500e743d0
Nick Escobedo <nick@nickescobedo.com>
2014-11-23 22:23:30 -0600

    Start of test cases and getting phpunit configured along with composer.

--- tests/pen-api/EchoNestConfigTest.php
+++ tests/pen-api/EchoNestConfigTest.php
@@ -3,6 +3,11 @@
 namespace nickescobedo\penapi;
 
 
-class EchoNestConfigTest extends TestCase{
+class EchoNestConfigTest extends \PHPUnit_Framework_TestCase{
+    public function testReturnFormat(){
+        $echoNestConfig = new EchoNestConfig();
+        $echoNestConfig->setReturnFormat('json');
 
+        $this->assertEquals('json', $echoNestConfig->getReturnFormat());
+    }
 } 
--- tests/pen-api/TestCase.php
+++ tests/pen-api/TestCase.php
@@ -3,6 +3,8 @@
 namespace nickescobedo\penapi;
 
 
-class TestCase extends \PHPUnit_TestCase{
+class TestCase extends \PHPUnit_Framework_TestCase{
+    public function woohoo(){
 
+    }
 } 
