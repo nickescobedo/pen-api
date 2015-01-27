db9023cf3c5a1bcc42aee9cf27c1df8eaad77a6d
Nick Escobedo <nick@nickescobedo.com>
2015-01-27 00:11:29 -0600

    Added function to parse data correctly.

--- src/pen-api/EchoNestResponse.php
+++ src/pen-api/EchoNestResponse.php
@@ -4,20 +4,75 @@ namespace nickescobedo\penapi;
 
 class EchoNestResponse implements ResponseInterface{
     protected $request;
-    protected $rawResponse;
+    protected $response;
     protected $body;
 
     public function __construct(RequestInterface $request, $data)
     {
         $this->request = $request;
-        $this->rawResponse = $data;
+        $this->response = $data;
         $this->decodeBody();
     }
 
     public function decodeBody()
     {
-        $this->body = $this->rawResponse->getBody();
+        if($this->isJson()){
+            $this->parseJsonResponse();
+        }
+        else if($this->isXml()){
+            $this->parseXmlResponse();
+        }
+        else{
+            throw new \Exception;
+        }
+
+        $this->parseResponseBodyForData();
         return $this->body;
+    }
+
+    private function isXml()
+    {
+        try {
+            $this->response->xml();
+        } catch(XmlParseException $e){
+            return false;
+        }
+
+        return true;
+    }
+
+    private function isJson()
+    {
+        try {
+            $this->response->json();
+        } catch(ParseException $e){
+            return false;
+        }
+
+        return true;
+    }
+
+    private function parseResponseBodyForData()
+    {
+        foreach($this->getBody() as $key => $value){
+            if($key != 'status'){
+                $this->data = $value;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function parseJsonResponse()
+    {
+        $body = json_decode($this->response->getBody());
+        $this->body = $body->response;
+    }
+
+    private function parseXmlResponse()
+    {
+        $this->body = $this->response->xml();
     }
 
     public function getRequest()
@@ -30,18 +85,46 @@ class EchoNestResponse implements ResponseInterface{
         return $this->body;
     }
 
+    public function getBody()
+    {
+        return $this->body;
+    }
+
     public function isSuccessful()
     {
-        // TODO: Implement isSuccessful() method.
+        $status = $this->body->status;
+
+        if($status->code == 0)
+        {
+            return true;
+        }
+
+        return false;
     }
 
     public function getErrors()
     {
-        // TODO: Implement getErrors() method.
+        $status = $this->body->status;
+        return $status->message;
     }
 
     public function getResponse()
     {
-        // TODO: Implement getResponse() method.
+        return $this->response;
+    }
+
+    public function getHeaders()
+    {
+        return $this->response->getHeaders();
+    }
+
+    public function getRateLimit()
+    {
+        return $this->response->getHeaders()['X-Ratelimit-Limit'];
+    }
+
+    public function getRateLimitUsed()
+    {
+        return $this->response->getHeaders()['X-Ratelimit-Used'];
     }
 }
