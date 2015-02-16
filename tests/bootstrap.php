d9208de5008a167e14756d43d8d131d7deba64e1
Nick Escobedo <nick@nickescobedo.com>
2015-02-15 20:21:21 -0600

    Fixed bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,18 @@
 <?php
 
-if (! @include_once __DIR__.'/../vendor/autoload.php') {
-    exit("You must set up the project dependencies, run the following commands:\n> wget http://getcomposer.org/composer.phar\n> php composer.phar install\n");
+if(!file_exists(__DIR__.'/../vendor/autoload.php')){
+    if(!file_exists(__DIR__.'/../../../autoload.php')){
+        echo 'You must set up the project dependencies, run the following commands:'.PHP_EOL.
+            'curl -sS https://getcomposer.org/installer | php'.PHP_EOL.
+            'php composer.phar install'.PHP_EOL;
+        exit(1);
+    }
+    else{
+        $autoload = __DIR__.'/../../../autoload.php';
+    }
 }
+else{
+    $autoload = __DIR__.'/../vendor/autoload.php';
+}
+
+require_once($autoload);
